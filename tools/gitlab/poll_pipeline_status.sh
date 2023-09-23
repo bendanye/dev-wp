75baa0e4e8796a1e1bfabee9faf05a320e580af8
bendanye <shen_soonng@hotmail.com>
2023-09-23 16:09:57 +0800

    Script to poll pipeline status

---
+++ tools/gitlab/poll_pipeline_status.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+PROJECT=$(basename $(pwd))
+
+set -eu
+
+source ../gitlab.env
+
+while true
+do
+    status=$(curl -s --header "PRIVATE-TOKEN: $ACCESS_TOKEN" "${GITLAB_URL}/api/v4/projects/${GROUP}${PROJECT}/pipelines" | jq first | jq -r '.status')
+    if [[ $status != "success" && $status != "failed" && $status != "canceled" ]]; then
+        echo "Still in progress. Sleep for 10 secs"
+        sleep 10
+    else
+        echo "No more running. Status is $status"
+        break
+    fi
+done
+
+# Can call another script to alert
