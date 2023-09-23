b2b235fa1ef960c6a44c76fee96488d08c43cbb7
bendanye <shen_soonng@hotmail.com>
2023-09-23 16:17:26 +0800

    Ci Linter

---
+++ tools/gitlab/ci_linter/ci_linter.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
+PROJECT=$(basename $(pwd))
+
+BASEDIR=$(dirname "$0")
+
+source ../../gitlab.env
+
+CONFIG_PATH="$BASEDIR/config.ini"
+
+files=$(find . -name ".gitlab-ci*.yml")
+
+if [[ -z $files ]]; then
+    echo "No .gitlab-ci files found"
+else
+    for file in $files
+    do
+        echo "Linting $file"
+        gitlab --config-file "$CONFIG_PATH" project-ci-lint validate --project-id $GROUP/$PROJECT --content @$file
+    done
+
+    echo "All .gitlab-ci files are lint without errors!"
+fi
+
