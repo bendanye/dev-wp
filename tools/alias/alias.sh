0acf36c2c28904838bd95488f6a9c423797740d1
bendanye <shen_soonng@hotmail.com>
2023-12-09 12:59:22 +0800

    Change alias

--- tools/alias/alias.sh
+++ tools/alias/alias.sh
@@ -20,6 +20,6 @@ alias gpst="$TOOLS_DIR/../gitlab/ci_linter/ci_linter.sh"
 
 alias pomot="$TOOLS_DIR/../pomodoro_timer/pomodoro_timer.sh"
 alias pomotst="$TOOLS_DIR/../pomodoro_timer/status.sh"
-alias pomotsum="$TOOLS_DIR/../pomodoro_timer/summary.sh"
+alias pomotdsum="$TOOLS_DIR/../pomodoro_timer/day_summary.sh"
 
 alias devwp="cd $TOOLS_DIR/../../"
