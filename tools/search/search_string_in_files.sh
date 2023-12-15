251e8df55d4980abbafa918f5df63d6606bc5212
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2023-12-15 13:43:29 +0800

    Update search_string_in_files.sh

--- tools/search/search_string_in_files.sh
+++ tools/search/search_string_in_files.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-# Check if the correct number of arguments is provided
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <FOLDER_PATH> <STRING_TO_SEARCH>"
+    echo "Usage: $0 <directory> <word>"
     exit 1
 fi
 
-FOLDER_PATH=$1
-STRING_TO_SEARCH=$2
+search_word_in_file() {
+    local file="$1"
+    local word="$2"
+    # Check if the file contains the specified word
+    if grep -q "$word" "$file"; then
+        echo "Found in: $file"
+    fi
+}
 
-# Use find to locate files in the specified folder and subdirectories, and grep to search for content
-find "$FOLDER_PATH" -type f -exec grep -l "$STRING_TO_SEARCH" {} + 2>/dev/null
+search_word_in_directory() {
+    local directory="$1"
+    local word="$2"
+    # Iterate through files in the directory
+    for file in "$directory"/*; do
+        if [ -d "$file" ]; then
+            # If it's a directory, recursively search it
+            search_word_in_directory "$file" "$word"
+        elif [ -f "$file" ]; then
+            # If it's a file, search for the word in it
+            search_word_in_file "$file" "$word"
+        fi
+    done
+}
+
+directory="$1"
+word="$2"
+if [ ! -d "$directory" ]; then
+    echo "Error: Directory '$directory' not found."
+    exit 1
+fi
+# Perform the search
+search_word_in_directory "$directory" "$word"
