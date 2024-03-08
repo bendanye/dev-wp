0ab5e05ed5744fad6363174769fccfd7768140ef
bendanye <shen_soonng@hotmail.com>
2024-03-08 19:55:28 +0800

    New scripts

---
+++ tools/gitlab/open_project_mr_reviewed.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eu
+
+PROJECT=$(basename `git rev-parse --show-toplevel`)
+
+cd "$(dirname "$0")"
+source ../gitlab.env
+
+open -a "Google Chrome" "${GITLAB_URL}/${GROUP}/$PROJECT/-/merge_requests?reviewer_username=${USER_NAME}"
