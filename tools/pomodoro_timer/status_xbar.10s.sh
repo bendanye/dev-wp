67b0a675aaeb40eea93ec57ea289bf40c2e570fe
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2024-04-01 11:58:14 +0800

    Display minutes in red if 25 minutes or more

--- tools/pomodoro_timer/status_xbar.10s.sh
+++ tools/pomodoro_timer/status_xbar.10s.sh
@@ -8,7 +8,15 @@ SCRIPT_DIR=$( dirname -- "$0"; )
 
 FILE="$SCRIPT_DIR/timer"
 if test -f "$FILE"; then
-    echo "🍅"
+    START=$(cat $FILE)
+    END=$(date +%s)
+    DURATION=$((END-START))
+    MINUTES=$(($DURATION / 60))
+    if [[ $MINUTES -ge 25 ]]; then
+        echo "$MINUTES | color=red"
+    else
+        echo "🍅"
+    fi
 else
     echo "⏹"
 fi 
