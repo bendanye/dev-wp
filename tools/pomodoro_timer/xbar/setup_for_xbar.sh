9295e742d3784557eb8f18a6a3e6fadb1a8ca41d
bendanye <shen_soonng@hotmail.com>
2024-04-06 13:46:22 +0800

    Move xbar related scripts to subfolder

--- tools/pomodoro_timer/xbar/setup_for_xbar.sh
+++ tools/pomodoro_timer/xbar/setup_for_xbar.sh
@@ -7,7 +7,7 @@ mkdir -p $SCRIPT_DIR/env
 echo "SOURCE_DIR=$SCRIPT_DIR" > $SCRIPT_DIR/env/pomodoro_timer.env
 
 cd ~/Library/Application\ Support/xbar/plugins
-ln -s "$SCRIPT_DIR"/status_xbar.10s.sh
+ln -s "$SCRIPT_DIR"/pomodoro_timer_status.10s.sh
 
 mkdir -p ~/Library/Application\ Support/xbar/plugins/env
 cd ~/Library/Application\ Support/xbar/plugins/env
