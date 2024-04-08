4b11ec5f56c1ded1eb52e9a4e3d85a3bcfdbb76d
bendanye <shen_soonng@hotmail.com>
2024-04-08 17:17:24 +0800

    Fixed path issue

--- tools/gitlab/monitor_gitlab_pipelines/setup_for_xbar.sh
+++ tools/gitlab/monitor_gitlab_pipelines/setup_for_xbar.sh
@@ -7,4 +7,4 @@ ln -s "$script_path"/gitlab_build.1m.sh
 
 mkdir -p ~/Library/Application\ Support/xbar/plugins/env
 cd ~/Library/Application\ Support/xbar/plugins/env
-ln -s "$script_path/../..//gitlab.env"
+ln -s "$script_path/../../gitlab.env"
