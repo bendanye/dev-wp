bc6074136abb24056e27e9c6bea36b43e3aba175
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2024-04-15 16:38:51 +0800

    Rename variable for consistency

--- tools/pomodoro_timer/pomodoro_timer.sh
+++ tools/pomodoro_timer/pomodoro_timer.sh
@@ -16,17 +16,17 @@ if test -f "$TIMER_FILE"; then
     TASK=$(cat $TIMER_FILE | cut -d ',' -f2)
     rm $TIMER_FILE
     END=$(date +%s)
-    secs=$((END-START))
-    working_time=$(( secs/60 ))
-    resting_time=$(( working_time/5 ))
-    rest_until=$(date "-v+$resting_time"M '+%H:%M')
+    SECS=$((END-START))
+    DESK_TIME=$(( SECS/60 ))
+    RESTING_TIME=$(( DESK_TIME/5 ))
+    REST_UNTIL=$(date "-v+$RESTING_TIME"M '+%H:%M')
 
     if [[ $TASK == "NIL" ]]; then
-        echo "Total minutes at my desk time: $working_time"
+        echo "Total minutes at my desk time: $DESK_TIME"
     else
-        echo "Total minutes at my desk time working on $TASK: $working_time"
+        echo "Total minutes at my desk time focusing on $TASK: $DESK_TIME"
     fi
-    echo "Please check the communication tools and take a break for $resting_time minutes until $rest_until"
+    echo "Please check the communication tools and take a break for $RESTING_TIME minutes until $REST_UNTIL"
 else
     clear
     echo "Start timer"
