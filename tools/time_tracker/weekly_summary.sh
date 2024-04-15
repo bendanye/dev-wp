0e07b18f5c964e51c04f94c4697dd5ca8c46d677
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2024-04-15 16:54:11 +0800

    Rename variable for consistency

--- tools/time_tracker/weekly_summary.sh
+++ tools/time_tracker/weekly_summary.sh
@@ -3,50 +3,50 @@
 SCRIPT_DIR=$( dirname -- "$0"; )
 
 # Get the current day of the week (1 for Monday, 2 for Tuesday, ..., 0 for Sunday)
-current_day=$(date +%u)
+CURRENT_DATE=$(date +%u)
 
 # Calculate the date for the start of the week (Monday)
-start_of_week=$(date -v -$((current_day - 1))d "+%Y-%m-%d")
+START_OF_WEEK=$(date -v -$((CURRENT_DATE - 1))d "+%Y-%m-%d")
 
 # Calculate the date for the end of the week (Sunday)
-end_of_week=$(date -v +$((6 - current_day + 1))d "+%Y-%m-%d")
+END_OF_WEEK=$(date -v +$((6 - CURRENT_DATE + 1))d "+%Y-%m-%d")
 
 # Loop through the dates from start to end of the week (inclusive)
-current_date=$start_of_week
-while [[ $(date -jf "%Y-%m-%d" "$current_date" "+%Y%m%d") -le $(date -jf "%Y-%m-%d" "$end_of_week" "+%Y%m%d") ]]; do
-    file="$SCRIPT_DIR/tracking_$current_date.txt"
+loop_date=$START_OF_WEEK
+while [[ $(date -jf "%Y-%m-%d" "$loop_date" "+%Y%m%d") -le $(date -jf "%Y-%m-%d" "$END_OF_WEEK" "+%Y%m%d") ]]; do
+    file="$SCRIPT_DIR/tracking_$loop_date.txt"
     if test -f "$file"; then
-        minutes=$(awk -F, '{if(NR==1)next;total+=$3}END{print total}' $file)
-        total_minutes=$(( total_minutes + minutes ))
+        MINUTES_IN_LOG=$(awk -F, '{if(NR==1)next;total+=$3}END{print total}' $file)
+        total_minutes=$(( total_minutes + MINUTES_IN_LOG ))
         total_days=$(( total_days + 1 ))
 
-        hours=$(( minutes/60 ))
-        min=$(( minutes-$hours*60 ))
-        echo "$current_date (1)  - $hours hours, $min minutes"
+        HOUR=$(( MINUTES_IN_LOG/60 ))
+        MINUTE=$(( MINUTES_IN_LOG-$HOUR*60 ))
+        echo "$loop_date (1)  - $HOUR hours, $MINUTE minutes"
     else
-        file="$SCRIPT_DIR/tracking_h_$current_date.txt"
+        file="$SCRIPT_DIR/tracking_h_$loop_date.txt"
         if test -f "$file"; then
-            minutes=$(awk -F, '{if(NR==1)next;total+=$3}END{print total}' $file)
-            total_minutes=$(( total_minutes + minutes ))
+            MINUTES_IN_LOG=$(awk -F, '{if(NR==1)next;total+=$3}END{print total}' $file)
+            total_minutes=$(( total_minutes + MINUTES_IN_LOG ))
             total_days=$(( total_days + .5 ))
 
-            hours=$(( minutes/60 ))
-            min=$(( minutes-$hours*60 ))
-            echo "$current_date (.5) - $hours hours, $min minutes"
+            HOUR=$(( MINUTES_IN_LOG/60 ))
+            min=$(( MINUTES_IN_LOG-$HOUR*60 ))
+            echo "$loop_date (.5) - $HOUR hours, $min minutes"
         fi
     fi
 
     # Add one day to the current date
-    current_date=$(date -jf "%Y-%m-%d" -v+1d "$current_date" "+%Y-%m-%d")
+    loop_date=$(date -jf "%Y-%m-%d" -v+1d "$loop_date" "+%Y-%m-%d")
 done
 
 if [ -z $total_days ]; then
-    echo "There is no tracking information from $start_of_week to $end_of_week"
+    echo "There is no tracking information from $START_OF_WEEK to $END_OF_WEEK"
     exit 0
 fi
 
 if (( total_days > 1 )); then
-   average_minutes=$(( total_minutes / total_days ))
+    average_minutes=$(( total_minutes / total_days ))
 else
     average_minutes=$total_minutes
 fi
@@ -54,7 +54,7 @@ fi
 # Remove decimal place
 average_minutes=${average_minutes%.*}
 
-average_hour=$(( average_minutes/60 ))
-average_min=$(( average_minutes-$average_hour*60 ))
+AVERAGE_HOUR=$(( average_minutes/60 ))
+AVERAGE_MIN=$(( average_minutes-$AVERAGE_HOUR*60 ))
 
-echo "On average ($total_days days), I am on my desk for $average_hour hours, $average_min minutes" 
+echo "On average ($total_days days), I am on my desk for $AVERAGE_HOUR hours, $AVERAGE_MIN minutes" 
