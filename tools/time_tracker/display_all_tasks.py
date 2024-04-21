130822b62fca8a65aaafed5a07698b46c30aafe8
bendanye <shen_soonng@hotmail.com>
2024-04-21 08:35:14 +0800

    Split showing into current week and the previous

--- tools/time_tracker/display_all_tasks.py
+++ tools/time_tracker/display_all_tasks.py
@@ -3,19 +3,32 @@ import os
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
-tasks = set()
+
+def get_tasks(files, exclude_tasks):
+    result = set()
+
+    for file_name in files:
+        with open(file_name, "r") as file:
+            next(file)
+            lines = file.readlines()
+            for line in lines:
+                data = line.replace("\n", "").split(",")
+                task = data[1]
+                if task not in exclude_tasks and task != "NIL":
+                    result.add(task)
+
+    return result
+
 
 files = glob.glob(f"{SCRIPT_DIR}/tracking_*.txt")
-files = files + glob.glob(f"{SCRIPT_DIR}/bkup/tracking_*.txt")
+this_week_tasks = get_tasks(files, set())
+print("This week's tasks that were focused on:")
+for task in sorted(this_week_tasks):
+    print(task)
 
-for file_name in files:
-    with open(file_name, "r") as file:
-        next(file)
-        lines = file.readlines()
-        for line in lines:
-            data = line.replace("\n", "").split(",")
-            tasks.add(data[1])
 
-for task in sorted(tasks):
-    if task != "NIL":
-        print(task)
+files = glob.glob(f"{SCRIPT_DIR}/bkup/tracking_*.txt")
+remaining_tasks = get_tasks(files, this_week_tasks)
+print("\nPrevious weeks' tasks that were focused on:")
+for task in sorted(remaining_tasks):
+    print(task)
