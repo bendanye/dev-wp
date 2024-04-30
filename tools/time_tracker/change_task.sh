2aa25001128223e64168c2ec24922b55dd4fa760
bendanye <shen_soonng@hotmail.com>
2024-04-30 13:20:43 +0800

    Added change existing task to specified one

--- tools/time_tracker/change_task.sh
+++ tools/time_tracker/change_task.sh
@@ -2,27 +2,48 @@
 
 SCRIPT_DIR=$( dirname -- "$0"; )
 
-TASK=$1
-if [[ -z $TASK ]]; then
-    echo "Please enter the new task to change"
+TYPE=$1
+if [[ -z $TYPE ]]; then
+    echo "Please enter the type (CURRENT | ALL)"
     exit 0
 fi
 
-ACTION=$2
+change_current_task() {
+    NEW_TASK=$1
+    if [[ -z $NEW_TASK ]]; then
+        echo "Please enter the new task to change"
+        exit 0
+    fi
 
-if [[ -z $ACTION ]]; then
-    ACTION="CHANGE"
-fi
-
-TIMER_FILE="$SCRIPT_DIR/../pomodoro_timer/timer"
-if [ ! -f "$TIMER_FILE" ]; then
-    echo "There is no timer running now"
-    exit 0
-fi
-
-CURRENT_TASK=$(cat $TIMER_FILE | cut -d ',' -f2)
-
-if [ $ACTION == "CHANGE" ]; then
+    TIMER_FILE="$SCRIPT_DIR/../pomodoro_timer/timer"
+    if [ ! -f "$TIMER_FILE" ]; then
+        echo "There is no timer running now"
+        exit 0
+    fi
+    CURRENT_TASK=$(cat $TIMER_FILE | cut -d ',' -f2)
     sed -i "" "s/$CURRENT_TASK/$TASK/g" $TIMER_FILE
-    echo "Updated task from $CURRENT_TASK to $TASK"
+    echo "Updated current task, $CURRENT_TASK to $NEW_TASK"
+}
+
+change_all_matching_task() {
+    OLD_TASK=$1
+    if [[ -z $OLD_TASK ]]; then
+        echo "Please enter the old task to change"
+        exit 0
+    fi
+
+    NEW_TASK=$2
+    if [[ -z $NEW_TASK ]]; then
+        echo "Please enter the new task to change"
+        exit 0
+    fi
+
+    find "$SCRIPT_DIR" -type f -name "tracking_*.txt" -exec sed -i "" -e "s/$OLD_TASK/$NEW_TASK/g" {} \;
+    echo "Updated all matching task, $OLD_TASK to $NEW_TASK"
+}
+
+if [ $TYPE == "CURRENT" ]; then
+    change_current_task $2
+else
+    change_all_matching_task $2 $3
 fi
