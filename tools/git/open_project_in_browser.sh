6e5e2635bb280d7afd526d95d67751b20df151ff
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2024-05-17 16:31:47 +0800

    only allow to open if starts with https

--- tools/git/open_project_in_browser.sh
+++ tools/git/open_project_in_browser.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-URL=$(git config --get remote.origin.url)
-
-open  -a "Google Chrome" $URL
+if [[ $URL == https* ]]; then
+    open  -a "Google Chrome" $URL
+else
+    echo "Unable to open. It Does not starts with https"
+fi
