7b735b0ef2d0a3d5ced5e0ef3b108f63503d6055
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2024-05-17 16:25:36 +0800

    Fixed when there is multiple groups

--- tools/gitlab/open_project.sh
+++ tools/gitlab/open_project.sh
@@ -6,8 +6,15 @@ else
     PROJECT=$(basename `git rev-parse --show-toplevel`)
 fi
 
+URL=$(git config --get remote.origin.url)
+
 cd "$(dirname "$0")"
 
 source ../gitlab.env
+
+if [[ $GITLAB_GROUP == "" ]]; then
+    GITLAB_GROUP=${URL#*:}
+    GITLAB_GROUP=${GITLAB_GROUP%/*}
+fi
 
 open  -a "Google Chrome" "${GITLAB_URL}/${GITLAB_GROUP}/${PROJECT}"
