7b96aa72993163047d359422dc93b1e06fa4467a
bendanye <shen_soonng@hotmail.com>
2024-08-24 21:49:27 +0800

    Added way to get keyword finder main file from source.env if set

--- tools/git/keyword_finder/run_keyword_finder.sh
+++ tools/git/keyword_finder/run_keyword_finder.sh
@@ -1,4 +1,10 @@
-KEYWORD_FINDER_MAIN_FILE=""
+SCRIPT_DIR="$(dirname "$0")"
+
+if [ -f "$SCRIPT_DIR/../source.env" ]; then
+    source "$SCRIPT_DIR/../source.env"
+else
+    KEYWORD_FINDER_MAIN_FILE=""
+fi
 
 if [[ $KEYWORD_FINDER_MAIN_FILE == "" ]]; then
     echo "Please specify the path and the file where keyword-finder project located"
