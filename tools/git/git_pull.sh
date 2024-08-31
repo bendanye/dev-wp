9c877aa6a81da9330d2cb1efc923adf1d1ea19af
bendanye <shen_soonng@hotmail.com>
2024-08-31 13:28:49 +0800

    Change the directory

--- tools/git/git_pull.sh
+++ tools/git/git_pull.sh
@@ -1,7 +1,17 @@
-repos=$(ls -d */)
-for repo_dir in $repos; do
-    echo $repo_dir
-    cd $repo_dir 
-    git pull
-    cd ..
+SCRIPT_DIR=$( dirname -- "$0"; )
+
+if [ -f "$SCRIPT_DIR/../source.env" ]; then
+    source "$SCRIPT_DIR/../source.env"
+    DIRECTORY=$GIT_PROJECT_DIR
+else
+    DIRECTORY=~/IdeaProjects
+fi
+
+for dir in "$DIRECTORY"/*; do
+    if [ -d "$dir/.git" ]; then
+        cd "$dir" || continue
+        PROJECT_NAME=$(basename "$PWD")
+        echo "git pull $PROJECT_NAME"
+        git pull
+    fi
 done
