a51e28615430d08744df16ea5316bb5eb50a4301
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2024-10-21 17:10:43 +0800

    Allow multiple criteria

--- tools/search/content_filter.py
+++ tools/search/content_filter.py
@@ -1,18 +1,39 @@
 from typing import List
+from dataclasses import dataclass
+
+@dataclass
+class Criteria:
+    substring: str
+    is_match_substring: bool
+
+    def is_match(self, line: str) -> bool:
+        if self.is_match_substring and self.substring in line:
+            return True
+        if not self.is_match_substring and not self.substring in line:
+            return True
+        
+        return False
 
 
-def get_lines(file_path: str, substring: str, match: bool = True):
+
+def get_lines(file_path: str, criterias: List[Criteria]):
     result = []
     with open(file_path, "r") as file:
         for line_number, line in enumerate(file, start=1):
             if not line.strip():
                 continue
-            if match and substring in line:
-                result.append(line.strip())
-            if not match and substring not in line:
+            if _is_all_criteria_met(line, criterias):
                 result.append(line.strip())
 
     return result
+
+def _is_all_criteria_met(line: str, criterias: List[Criteria]) -> bool:
+    for criteria in criterias:
+        if not criteria.is_match(line):
+            return False
+        
+    return True
+
 
 
 def _output_result(output_file_path: str, lines: List[str]):
@@ -22,9 +43,12 @@ def _output_result(output_file_path: str, lines: List[str]):
 
 
 if __name__ == "__main__":
-    input_file_path = "test.log"
-    substring = "i am"
-    output_file_path = "hello.log"
+    input_file_path = "hello.csv"
+    output_file_path = "hello_output.csv"
 
-    result = get_lines(input_file_path, substring, False)
+    criterias = []
+    criterias.append(Criteria(substring=";True;", is_match_substring=True))
+    criterias.append(Criteria(substring=";False", is_match_substring=True))
+
+    result = get_lines(input_file_path, criterias)
     _output_result(output_file_path, result)
