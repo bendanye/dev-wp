226f14ada214ae2440809637bc767018aa6a4c1e
bendanye <shen_soonng@hotmail.com>
2024-12-14 14:22:43 +0800

    Script to create new work log from a template

---
+++ work_logs/create_new_worklog.py
@@ -0,0 +1,63 @@
+import os.path
+
+from datetime import timedelta, date
+
+today = date.today()
+days_since_monday = today.weekday()
+
+monday = today - timedelta(days=days_since_monday)
+
+if os.path.isfile(f'{monday.strftime("%Y%m%d")}.md'):
+    print("This week work log is already exists. Skip creating")
+else:
+    with open("template.md", "r") as fin:
+        with open(f'{monday.strftime("%Y%m%d")}.md', "w") as fout:
+            for line in fin:
+                if "## Week of [Month Day, Year]" in line:
+                    fout.write(
+                        line.replace(
+                            "## Week of [Month Day, Year]",
+                            f'## Week of [{monday.strftime("%d %m, %Y")}]',
+                        )
+                    )
+                elif "### Monday, [Date]" in line:
+                    fout.write(
+                        line.replace(
+                            "### Monday, [Date]",
+                            f'### Monday, [{monday.strftime("%Y-%m-%d")}]',
+                        )
+                    )
+                elif "### Tuesday, [Date]" in line:
+                    tuesday = monday + timedelta(+1)
+                    fout.write(
+                        line.replace(
+                            "### Tuesday, [Date]",
+                            f'### Tuesday, [{tuesday.strftime("%Y-%m-%d")}]',
+                        )
+                    )
+                elif "### Wednesday, [Date]" in line:
+                    wednesday = monday + timedelta(+2)
+                    fout.write(
+                        line.replace(
+                            "### Wednesday, [Date]",
+                            f'### Wednesday, [{wednesday.strftime("%Y-%m-%d")}]',
+                        )
+                    )
+                elif "### Thursday, [Date]" in line:
+                    thursday = monday + timedelta(+3)
+                    fout.write(
+                        line.replace(
+                            "### Thursday, [Date]",
+                            f'### Thursday, [{thursday.strftime("%Y-%m-%d")}]',
+                        )
+                    )
+                elif "### Friday, [Date]" in line:
+                    friday = monday + timedelta(+4)
+                    fout.write(
+                        line.replace(
+                            "### Friday, [Date]",
+                            f'### Friday, [{friday.strftime("%Y-%m-%d")}]',
+                        )
+                    )
+                else:
+                    fout.write(line)
