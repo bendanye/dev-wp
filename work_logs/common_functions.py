fb4d947a973847526a7324a3d4d5fc63b826bd72
bendanye <shen_soonng@hotmail.com>
2024-12-25 14:05:29 +0800

    New function to get last working date

--- work_logs/common_functions.py
+++ work_logs/common_functions.py
@@ -3,3 +3,14 @@ from datetime import timedelta
 
 def get_last_monday(specified_date):
     return specified_date - timedelta(days=specified_date.weekday())
+
+
+def get_last_working_date(specified_date):
+    weekday = specified_date.weekday()
+
+    if weekday == 0:
+        last_working_day = specified_date - timedelta(days=3)
+    else:
+        last_working_day = specified_date - timedelta(days=1)
+
+    return last_working_day
--- work_logs/test_common_functions.py
+++ work_logs/test_common_functions.py
@@ -1,6 +1,6 @@
 import pytest
 
-from common_functions import get_last_monday
+from common_functions import get_last_monday, get_last_working_date
 from datetime import datetime
 
 
@@ -14,3 +14,11 @@ from datetime import datetime
 )
 def test_get_last_monday_should_get(specified_date, expected_date):
     assert expected_date == get_last_monday(specified_date)
+
+
+def test_get_last_working_date_should_get_yesterday():
+    assert get_last_working_date(datetime(2024, 12, 10)) == datetime(2024, 12, 9)
+
+
+def test_get_last_working_date_should_get_friday_when_today_is_monday():
+    assert get_last_working_date(datetime(2024, 12, 9)) == datetime(2024, 12, 6)
