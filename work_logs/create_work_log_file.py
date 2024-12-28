44b8812982336df9d74b2e5f213e266cb13b2d62
bendanye <shen_soonng@hotmail.com>
2024-12-28 17:02:50 +0800

    Use new format function

--- work_logs/create_work_log_file.py
+++ work_logs/create_work_log_file.py
@@ -1,16 +1,22 @@
 import os.path
 
-from date_utils import get_last_monday
+from date_utils import (
+    get_last_monday,
+    format_date_to_yyyymmdd,
+    format_date_to_yyyymmdd_hyphen,
+)
 
 from datetime import timedelta, date
 
 monday = get_last_monday(date.today())
 
-if os.path.isfile(f'{monday.strftime("%Y%m%d")}.md'):
+work_log_file_name = format_date_to_yyyymmdd(monday)
+
+if os.path.isfile(f"{work_log_file_name}.md"):
     print("This week work log is already exists. Skip creating")
 else:
     with open("template.md", "r") as fin:
-        with open(f'{monday.strftime("%Y%m%d")}.md', "w") as fout:
+        with open(f"{work_log_file_name}.md", "w") as fout:
             for line in fin:
                 if "## Week of [Month Day, Year]" in line:
                     fout.write(
@@ -23,7 +29,7 @@ else:
                     fout.write(
                         line.replace(
                             "### Monday, [Date]",
-                            f'### Monday, [{monday.strftime("%Y-%m-%d")}]',
+                            f"### Monday, [{format_date_to_yyyymmdd_hyphen(monday)}]",
                         )
                     )
                 elif "### Tuesday, [Date]" in line:
@@ -31,7 +37,7 @@ else:
                     fout.write(
                         line.replace(
                             "### Tuesday, [Date]",
-                            f'### Tuesday, [{tuesday.strftime("%Y-%m-%d")}]',
+                            f"### Tuesday, [{format_date_to_yyyymmdd_hyphen(tuesday)}]",
                         )
                     )
                 elif "### Wednesday, [Date]" in line:
@@ -39,7 +45,7 @@ else:
                     fout.write(
                         line.replace(
                             "### Wednesday, [Date]",
-                            f'### Wednesday, [{wednesday.strftime("%Y-%m-%d")}]',
+                            f"### Wednesday, [{format_date_to_yyyymmdd_hyphen(wednesday)}]",
                         )
                     )
                 elif "### Thursday, [Date]" in line:
@@ -47,7 +53,7 @@ else:
                     fout.write(
                         line.replace(
                             "### Thursday, [Date]",
-                            f'### Thursday, [{thursday.strftime("%Y-%m-%d")}]',
+                            f"### Thursday, [{format_date_to_yyyymmdd_hyphen(thursday)}]",
                         )
                     )
                 elif "### Friday, [Date]" in line:
@@ -55,7 +61,7 @@ else:
                     fout.write(
                         line.replace(
                             "### Friday, [Date]",
-                            f'### Friday, [{friday.strftime("%Y-%m-%d")}]',
+                            f"### Friday, [{format_date_to_yyyymmdd_hyphen(friday)}]",
                         )
                     )
                 else:
