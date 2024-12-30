617f703a8035cfa206b504b75b32439b14620bfe
bendanye <shen_soonng@hotmail.com>
2024-12-30 09:02:14 +0800

    New script to extract learnings from all logs

---
+++ work_logs/extract_learnings.py
@@ -0,0 +1,26 @@
+import os
+
+directory = "."
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f) and "template.md" not in f and f.endswith(".md"):
+        with open(f, "r") as file:
+            lines = []
+            save_line = False
+            for line in file:
+                if "#### 💡 Learnings" in line:
+                    save_line = True
+
+                elif "### " in line and save_line is True:
+                    save_line = False
+
+                if save_line and (
+                    "#### 💡 Learnings" not in line
+                    and "- [ ]" not in line
+                    and "\n" != line
+                ):
+                    lines.append(line)
+
+            for line in lines:
+                print(line)
