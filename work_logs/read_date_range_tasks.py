2daf2ff9a8c8463384df9c093d9d1683dc4dc215
bendanye <shen_soonng@hotmail.com>
2024-12-30 09:36:20 +0800

    New script to get task by specified task and date range

---
+++ work_logs/read_date_range_tasks.py
@@ -0,0 +1,20 @@
+from datetime import timedelta, datetime
+
+from date_utils import format_date_to_yyyymmdd_hyphen
+from read_utils import get_specified_tasks
+
+DIRECTORY = "examples"
+
+start_date = datetime(2024, 12, 23)
+end_date = datetime(2024, 12, 27)
+specified_task = "ABCD-1234"
+
+loop_date = start_date
+
+while loop_date < end_date:
+    loop_date = loop_date + timedelta(days=1)
+    tasks = get_specified_tasks(DIRECTORY, loop_date, specified_task)
+    if tasks:
+        print(format_date_to_yyyymmdd_hyphen(loop_date))
+        for task in tasks:
+            print(task)
