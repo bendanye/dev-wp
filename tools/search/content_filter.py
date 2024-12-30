986473fdeb68bd59d1ab83d325338d0a517b8a8e
bendanye <shen_soonng@hotmail.com>
2024-12-30 17:39:27 +0800

    Refactor codes

--- tools/search/content_filter.py
+++ tools/search/content_filter.py
@@ -23,6 +23,20 @@ class Line:
     content: str
 
 
+def main(
+    criterias: List[Criteria],
+    input_path: str,
+    output_file_path: str,
+    show_all_details: bool = False,
+) -> None:
+    lines = get_lines(input_path, criterias)
+
+    if output_file_path:
+        _output_to_file(output_file_path, lines, show_all_details)
+    else:
+        _output_to_terminal(lines, show_all_details)
+
+
 def get_lines(file_path: str, criterias: List[Criteria]) -> List[Line]:
     result = []
     with open(file_path, "r") as file:
@@ -45,10 +59,10 @@ def _is_all_criteria_met(line: str, criterias: List[Criteria]) -> bool:
     return True
 
 
-def _output_result(output_file_path: str, lines: List[Line], save_all: bool):
+def _output_to_file(output_file_path: str, lines: List[Line], show_all_detail: bool):
     with open(output_file_path, "w") as file:
         for line in lines:
-            if save_all:
+            if show_all_detail:
                 file.write(
                     f"File={line.file_path};Line number={line.number};{line.content}\n"
                 )
@@ -56,13 +70,20 @@ def _output_result(output_file_path: str, lines: List[Line], save_all: bool):
                 file.write(f"{line.content}\n")
 
 
+def _output_to_terminal(lines: List[Line], show_all_detail: bool):
+    for line in lines:
+        if show_all_detail:
+            print(f"File={line.file_path};Line number={line.number};{line.content}")
+        else:
+            print(line.content)
+
+
 if __name__ == "__main__":
-    input_file_path = "examples/example1.csv"
+    input_path = "examples/example1.csv"
     output_file_path = "examples/example1_output.csv"
 
     criterias = []
     criterias.append(Criteria(substring=";True;", is_match_substring=True))
     criterias.append(Criteria(substring=";False", is_match_substring=True))
 
-    lines = get_lines(input_file_path, criterias)
-    _output_result(output_file_path, lines, save_all=False)
+    main(criterias, input_path, output_file_path, show_all_details=False)
