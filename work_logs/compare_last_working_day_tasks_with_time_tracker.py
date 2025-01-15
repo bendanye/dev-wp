13789bed580402e9c4e40ee36e2ff5f995e2d8ae
bendanye <shen_soonng@hotmail.com>
2025-01-15 17:42:15 +0800

    Show missing either time tracker or work log is empty

--- work_logs/compare_last_working_day_tasks_with_time_tracker.py
+++ work_logs/compare_last_working_day_tasks_with_time_tracker.py
@@ -7,49 +7,83 @@ from read_utils import get_tasks
 DIRECTORY = "logs"
 TASK_PREFIX = ""
 
-work_log_tasks = set()
 
-last_working_date = get_last_working_date(datetime.now())
-tasks = get_tasks(DIRECTORY, last_working_date)
+def main() -> None:
+    last_working_date = _get_last_working_date_exists()
+    work_log_tasks = _get_tasks_from_work_log(last_working_date)
+    time_tracker_tasks = _get_tasks_from_time_tracker(last_working_date)
 
-if not tasks:
-    while not tasks:
-        last_working_date = get_last_working_date(last_working_date)
-        tasks = get_tasks(DIRECTORY, last_working_date)
+    print(f"last working date: {format_date_to_yyyymmdd_hyphen(last_working_date)}")
+    if work_log_tasks and not time_tracker_tasks:
+        print("Missing time tracker tasks")
+    elif not work_log_tasks and time_tracker_tasks:
+        print("Missing work log tasks")
+    else:
+        missing = set()
+        for task in time_tracker_tasks:
+            if task.startswith(TASK_PREFIX) and task not in work_log_tasks:
+                missing.add(task)
 
-for task in tasks:
-    if "- [" in task:
-        work_log_tasks.add(
-            task.replace("- ", "").replace("[", "").split("]")[0].strip()
-        )
+        for task in work_log_tasks:
+            if task.startswith(TASK_PREFIX) and task not in time_tracker_tasks:
+                missing.add(task)
 
-time_tracker_file = f"../tools/time_tracker/tracking_{format_date_to_yyyymmdd_hyphen(last_working_date)}.txt"
-if not os.path.exists(time_tracker_file):
-    time_tracker_file = f"../tools/time_tracker/bkup/tracking_{format_date_to_yyyymmdd_hyphen(last_working_date)}.txt"
-
-time_tracker_tasks = set()
-with open(time_tracker_file) as file:
-    next(file)
-    task_lines = file.readlines()
-    for line in task_lines:
-        data = line.replace("\n", "").split(",")
-        task = data[1]
-        if task.startswith(TASK_PREFIX):
-            time_tracker_tasks.add(task)
+        if not missing:
+            print("No differences between work log and time tracker")
+        else:
+            print("There is differences between work log and time tracker")
+            print(missing)
 
 
-missing = set()
-for task in time_tracker_tasks:
-    if task.startswith(TASK_PREFIX) and task not in work_log_tasks:
-        missing.add(task)
+def _get_last_working_date_exists():
+    last_working_date = get_last_working_date(datetime.now())
+    while True:
+        work_log_tasks = get_tasks(DIRECTORY, last_working_date)
+        time_tracker_tasks = _get_tasks_from_time_tracker(last_working_date)
 
-for task in work_log_tasks:
-    if task.startswith(TASK_PREFIX) and task not in time_tracker_tasks:
-        missing.add(task)
+        if not work_log_tasks and not time_tracker_tasks:
+            last_working_date = get_last_working_date(last_working_date)
+        else:
+            break
+
+    return last_working_date
 
 
-if len(missing) == 0:        
-    print("No differences between work log and time tracker")
-else:
-    print("\033[91mHas differences between work log and time tracker")
-    print(missing)
+def _get_tasks_from_work_log(last_working_date):
+    work_log_tasks = set()
+
+    last_working_date = _get_last_working_date_exists()
+    tasks = get_tasks(DIRECTORY, last_working_date)
+
+    for task in tasks:
+        if TASK_PREFIX in task:
+            work_log_tasks.add(
+                task.replace("- ", "").replace("[", "").split("]")[0].strip()
+            )
+
+    return work_log_tasks
+
+
+def _get_tasks_from_time_tracker(last_working_date):
+    time_tracker_file = f"../tools/time_tracker/tracking_{format_date_to_yyyymmdd_hyphen(last_working_date)}.txt"
+    if not os.path.exists(time_tracker_file):
+        time_tracker_file = f"../tools/time_tracker/bkup/tracking_{format_date_to_yyyymmdd_hyphen(last_working_date)}.txt"
+
+    if not os.path.exists(time_tracker_file):
+        return set()
+
+    with open(time_tracker_file) as file:
+        next(file)
+        time_tracker_tasks = set()
+        task_lines = file.readlines()
+        for line in task_lines:
+            data = line.replace("\n", "").split(",")
+            task = data[1]
+            if task.startswith(TASK_PREFIX):
+                time_tracker_tasks.add(task)
+
+        return time_tracker_tasks
+
+
+if __name__ == "__main__":
+    main()
