6dcb405959c22ffcb54162fcb2c855eb914d8563
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2025-01-22 07:56:06 +0800

    Update daily_check.sh

--- work_logs/daily_check.sh
+++ work_logs/daily_check.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
 result=$(python3 compare_last_working_day_tasks_with_time_tracker.py)
 echo $result
 if [[ $result == *"Missing work log tasks"* || $result == *"There is differences between work log and time tracker"* ]]; then
-    echo "To add todo"
+  MESSAGE=$result
+  if [[ $TODO_MAIN_FILE != "" ]]; then
+    sh "${TODO_MAIN_FILE}" "$MESSAGE"
+  elif [[ $ALERTME_MAIN_FILE != "" ]]; then
+      sh $ALERTME_MAIN_FILE -m "$MESSAGE"
+  fi
 fi
 
