3e3bd564fdbe6c5cfbb60a10dacc62a6cd5da534
bendanye <shen_soonng@hotmail.com>
2025-02-01 15:06:03 +0800

    Include the last working date

--- work_logs/test_task_utils.py
+++ work_logs/test_task_utils.py
@@ -7,33 +7,41 @@ WORK_LOG_DIRECTORY = "examples"
 
 def test_get_last_working_day_tasks_should_get_yesterday_when_not_empty():
     specified_date = datetime(2024, 12, 25)
-    assert get_last_working_day_tasks(WORK_LOG_DIRECTORY, specified_date) == [
-        "\n",
-        "\n",
-        "- [ABCD-1234] testing\n",
-        "\n",
-        "  - i worked on testing portion\n",
-        "\n",
-        "- [ABCD-2233] testing 22\n",
-        "\n",
-        "  - i checked on how testing 22 outcome\n",
-        "\n",
-        "- troubleshooting something\n",
-    ]
+    expected_last_working_day = datetime(2024, 12, 24)
+    assert get_last_working_day_tasks(WORK_LOG_DIRECTORY, specified_date) == (
+        expected_last_working_day,
+        [
+            "\n",
+            "\n",
+            "- [ABCD-1234] testing\n",
+            "\n",
+            "  - i worked on testing portion\n",
+            "\n",
+            "- [ABCD-2233] testing 22\n",
+            "\n",
+            "  - i checked on how testing 22 outcome\n",
+            "\n",
+            "- troubleshooting something\n",
+        ],
+    )
 
 
 def test_get_last_working_day_tasks_should_get_the_last_working_day_when_yesterday_is_empty():
     specified_date = datetime(2024, 12, 26)
-    assert get_last_working_day_tasks(WORK_LOG_DIRECTORY, specified_date) == [
-        "\n",
-        "\n",
-        "- [ABCD-1234] testing\n",
-        "\n",
-        "  - i worked on testing portion\n",
-        "\n",
-        "- [ABCD-2233] testing 22\n",
-        "\n",
-        "  - i checked on how testing 22 outcome\n",
-        "\n",
-        "- troubleshooting something\n",
-    ]
+    expected_last_working_day = datetime(2024, 12, 24)
+    assert get_last_working_day_tasks(WORK_LOG_DIRECTORY, specified_date) == (
+        expected_last_working_day,
+        [
+            "\n",
+            "\n",
+            "- [ABCD-1234] testing\n",
+            "\n",
+            "  - i worked on testing portion\n",
+            "\n",
+            "- [ABCD-2233] testing 22\n",
+            "\n",
+            "  - i checked on how testing 22 outcome\n",
+            "\n",
+            "- troubleshooting something\n",
+        ],
+    )
