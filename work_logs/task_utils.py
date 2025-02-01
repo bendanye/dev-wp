9cecfa1285cb86d2af119820f654db086ff6ff07
bendanye <shen_soonng@hotmail.com>
2025-02-01 15:03:38 +0800

    New function to get last working day tasks

---
+++ work_logs/task_utils.py
@@ -0,0 +1,13 @@
+from read_utils import get_tasks
+from date_utils import get_last_working_date
+
+
+def get_last_working_day_tasks(work_log_directory, specified_date):
+    last_working_date = get_last_working_date(specified_date)
+    tasks = get_tasks(work_log_directory, last_working_date)
+    if not tasks:
+        while not tasks:
+            last_working_date = get_last_working_date(last_working_date)
+            tasks = get_tasks(work_log_directory, last_working_date)
+
+    return tasks
