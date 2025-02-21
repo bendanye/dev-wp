f412da2ef8427916feb7ca8de82daf21cf62e885
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2025-02-21 12:06:43 +0800

    Added open gitlab project mr page alias

--- tools/alias/alias.sh
+++ tools/alias/alias.sh
@@ -58,6 +58,7 @@ alias jcua="$TOOLS_DIR/../jira/jira_upload_attachment.sh"
 if [ -f "$TOOLS_DIR/../gitlab.env" ]; then
     alias gop="$TOOLS_DIR/../gitlab/open_project_page.sh PROJECT"
     alias gopb="$TOOLS_DIR/../gitlab/open_project_page.sh PROJECT_BRANCH"
+    alias gopmr="$TOOLS_DIR/../gitlab/open_project_page.sh MR"
     alias gopmra="$TOOLS_DIR/../gitlab/open_project_page.sh MR_ASSIGNED"
     alias gopmrr="$TOOLS_DIR/../gitlab/open_project_page.sh MR_REVIEWED"
     alias gopp="$TOOLS_DIR/../gitlab/open_project_page.sh PIPELINE"
