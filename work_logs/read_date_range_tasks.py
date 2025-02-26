97429630b1faa2c2dfc13a8a8d8344f8daaa4485
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2025-02-26 13:09:44 +0800

    Update read_date_range_tasks.py

--- work_logs/read_date_range_tasks.py
+++ work_logs/read_date_range_tasks.py
@@ -13,7 +13,12 @@ loop_date = start_date
 
 while loop_date < end_date:
     loop_date = loop_date + timedelta(days=1)
-    tasks = get_specified_tasks(DIRECTORY, loop_date, specified_task)
+    
+    if specified_task:
+        tasks = get_specified_tasks(DIRECTORY, loop_date, specified_task)
+    else:
+        tasks = get_tasks(DIRECTORY, loop_date)
+        
     if tasks:
         print(format_date_to_yyyymmdd_hyphen(loop_date))
         for task in tasks:
