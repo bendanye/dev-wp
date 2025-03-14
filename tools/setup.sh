ce6c376c456fd4994078387d16dcbaa058334059
bendanye <shen_soonng@hotmail.com>
2025-03-14 17:23:07 +0800

    Added code to setup tools using keyword finder

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_PATH=$(pwd)
 
+echo "Setup tools"
+
 read -p "Name of the project: " project_name
 read -p "Communication (google, slack, telegram, discord): " communication
 read -p "Name of the type of managment (default is cards): " management
@@ -28,5 +30,13 @@ echo "COMMUNICATION=(\"$communication\")" >> "$SCRIPT_PATH/tools.env"
 echo "GIT_PROJECT_DIR=\"$GIT_PROJECT_DIR\"" >> "$SCRIPT_PATH/tools.env"
 
 touch $project_dir/list.txt
+
+echo "Setup Keyword Finder"
+KEYWORD_FINDER_PROJECTS=("alias" "card_management" "git" "time_tracker")
+
+for project in ${KEYWORD_FINDER_PROJECTS[@]}; do
+    cd $SCRIPT_PATH/$project/keyword_finder
+    sh setup_keyword_finder.sh
+done
 
 echo "Setup completed"
