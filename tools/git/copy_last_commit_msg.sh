103c5d7583da8a3bd2e1ec5082d203be8ec55d31
bendanye <shen_soonng@hotmail.com>
2025-03-15 14:19:59 +0800

    use current directory as project if never specified

--- tools/git/copy_last_commit_msg.sh
+++ tools/git/copy_last_commit_msg.sh
@@ -4,13 +4,17 @@ SCRIPT_DIR=$( dirname -- "$0"; )
 
 if [ -n "$1" ]; then
     PROJECT=$1
-    if [ -f "$SCRIPT_DIR/../tools.env" ]; then
-        source "$SCRIPT_DIR/../tools.env"
-    else
-        GIT_PROJECT_DIR="$HOME/IdeaProjects"
-    fi
-    cd $GIT_PROJECT_DIR/$PROJECT
+else
+    PROJECT=$(basename `git rev-parse --show-toplevel`)
 fi
+
+if [ -f "$SCRIPT_DIR/../tools.env" ]; then
+    source "$SCRIPT_DIR/../tools.env"
+else
+    GIT_PROJECT_DIR="$HOME/IdeaProjects"
+fi
+
+cd $GIT_PROJECT_DIR/$PROJECT
 
 LAST_COMMIT_MSG=$(git log -1 --pretty=%B | cat | tr '\n' ' ')
 echo "Copied: $LAST_COMMIT_MSG"
