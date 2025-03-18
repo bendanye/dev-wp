87cc2d304e392efe59c848108a08b6d43f22a99d
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2025-03-18 09:36:32 +0800

    Update new_card.sh

--- tools/card_management/new_card.sh
+++ tools/card_management/new_card.sh
@@ -11,12 +11,9 @@ test "$1"
 ID=$1
 NEW_DIR="$WORKING_DIR/$ID"
 
-if [[ -d "$NEW_DIR" ]]; then
-    echo "$NEW_DIR already exists. Will exit"
-    exit 1
+if [[ ! -d "$NEW_DIR" ]]; then
+    mkdir "$NEW_DIR"
+    echo "Created new folder, $NEW_DIR"
 fi
-
-mkdir "$NEW_DIR"
-echo "Created new folder, $NEW_DIR"
 
 mkdir "$NEW_DIR/$CARD_TEST_EVIDENCES"
