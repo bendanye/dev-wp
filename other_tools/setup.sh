af4aa199313fe6428ea1615e7fd86089d3081532
bendanye <shen_soonng@hotmail.com>
2025-03-22 15:03:34 +0800

    Script to git clone extra tools

---
+++ other_tools/setup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/bendanye/renotes.git
+git cline https://github.com/bendanye/keyword-finder.git
+git clone https://github.com/bendanye/task-todo.git
+
+# optional
+# git clone https://github.com/bendanye/chrome-extensions.git
+# git clone https://github.com/bendanye/yet-another-dev-productivity-tools.git
