650e27bbb14babce1a229348d1e1b560806c0f59
bendanye <shen_soonng@hotmail.com>
2025-05-17 16:20:19 +0800

    Rename to make it more explictly to mac

--- tools/morning_starter/setup_mac.sh
+++ tools/morning_starter/setup_mac.sh
@@ -4,10 +4,10 @@ script_path=$(pwd)
 
 cd ~/Desktop
 
-ln -s "$script_path"/morning.command
+ln -s "$script_path"/morning_mac.command
 
-chmod 777 morning.command
+chmod 777 morning_mac.command
 
-ln -s "$script_path"/morning_dev.command
+ln -s "$script_path"/morning_mac_dev.command
 
-chmod 777 morning_dev.command
+chmod 777 morning_mac_dev.command
