febd9093f6033284dae9bda1961ea0004fe85ce0
bendanye <shen_soonng@hotmail.com>
2025-06-08 16:36:53 +0800

    Handle windows

--- tools/time_tracker/status.py
+++ tools/time_tracker/status.py
@@ -1,9 +1,13 @@
 import os
 import time
 import sys
-import termios
-import tty
-import select
+
+if os.name == "nt":
+    import msvcrt
+else:
+    import termios
+    import tty
+    import select
 
 
 def main():
@@ -39,17 +43,23 @@ def main():
 
 
 def input_timeout(timeout):
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(fd)
-        rlist, _, _ = select.select([sys.stdin], [], [], timeout)
-        if rlist:
-            return sys.stdin.read(1)
-        else:
-            raise TimeoutError
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    if os.name == "nt":
+        start_time = time.time()
+        while (time.time() - start_time) < timeout:
+            if msvcrt.kbhit():  # Check if a key has been pressed
+                return msvcrt.getch().decode("utf-8")  # Read the key and decode it
+    else:
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            rlist, _, _ = select.select([sys.stdin], [], [], timeout)
+            if rlist:
+                return sys.stdin.read(1)
+            else:
+                raise TimeoutError
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
 
 if __name__ == "__main__":
