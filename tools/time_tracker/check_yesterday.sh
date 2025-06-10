88f7664b3819ef076a6bba27b816cc24eabc8111
bendanye <shen_soonng@hotmail.com>
2025-06-10 18:39:09 +0800

    Removed not needed source

--- tools/time_tracker/check_yesterday.sh
+++ tools/time_tracker/check_yesterday.sh
@@ -3,7 +3,6 @@
 SCRIPT_DIR=$( dirname -- "$0"; )
 
 source "$SCRIPT_DIR/time_tracker_func.sh"
-source "$SCRIPT_DIR/../tools.env"
 
 if [ -f "$SCRIPT_DIR/../../dwp.env" ]; then
     source "$SCRIPT_DIR/../../dwp.env"
