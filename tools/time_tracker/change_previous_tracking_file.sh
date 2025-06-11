01eea99b1b682d9006821e160520d777ebb8dae8
bendanye <shen_soonng@hotmail.com>
2025-06-11 18:45:28 +0800

    Fixed date os compatible for windows and mac

--- tools/time_tracker/change_previous_tracking_file.sh
+++ tools/time_tracker/change_previous_tracking_file.sh
@@ -4,7 +4,14 @@ SCRIPT_DIR=$( dirname -- "$0"; )
 
 source "$SCRIPT_DIR/time_tracker_func.sh"
 
-CURRENT_DATE=$(date -v-1d '+%Y-%m-%d')
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  CURRENT_DATE=$(date -v -1d +%Y-%m-%d)
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin"* ]]; then
+  CURRENT_DATE=$(date -d "-1 day" "+%Y-%m-%d")
+else
+  echo "Unsupported operating system"
+  exit 1
+fi
 
 data_file=$(get_file $CURRENT_DATE)
 if ! test -f "$data_file"; then
