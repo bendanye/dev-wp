0ca1b1c5aeb854cc0da0511cc165a8adc1aa8bda
bendanye <shen_soonng@hotmail.com>
2025-06-28 14:32:42 +0800

    Added some links as optional

--- other_tools/setup.sh
+++ other_tools/setup.sh
@@ -1,7 +1,9 @@
 git clone https://github.com/bendanye/renotes.git
-git clone https://github.com/bendanye/keyword-finder.git
-git clone https://github.com/bendanye/task-todo.git
 
 # optional
 # git clone https://github.com/bendanye/chrome-extensions.git
 # git clone https://github.com/bendanye/yet-another-dev-productivity-tools.git
+# git clone https://github.com/bendanye/keyword-finder.git
+# git clone https://github.com/bendanye/task-todo.git
+# git clone https://github.com/bendanye/public-holiday
+# git clone https://github.com/bendanye/files-management
