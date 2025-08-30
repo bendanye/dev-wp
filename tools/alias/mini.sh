690448c8f16983897a89c01df75a66fe843a05be
bendanye <shen_soonng@hotmail.com>
2025-08-30 12:56:50 +0800

    Mini set of alias to used in enclosed environment

---
+++ tools/alias/mini.sh
@@ -0,0 +1,15 @@
+# when using Git Bash and without the whole dev_wp project, use this instead
+
+alias g="g"
+alias gp="git push"
+alias gl="git pull"
+alias gcl="git clone"
+alias gst="git status"
+alias gulc="git reset --soft HEAD~1"
+alias glo="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias ghm="cd \"\$(git rev-parse --show-toplevel)\""
+
+alias ..="cd .."
+
+alias cfn='basename `pwd` | clip'
+alias ccp='echo `pwd` | clip'
